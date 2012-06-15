a37ec43e8481aa9f89de98ac2fb89f1932074a24
Oscar Colomé <amrothceleb@gmail.com>
2012-06-15 18:49:59 +0200

    Camera no funcionava correctament
    Al posar el terreny de nou he tingut un petit problema amb la càmera. La
    graella s'instanciava sempre al mateix lloc ( a un racó del mapa. )
    Solucionat.

--- AlphaDemo/Scripts/TancarTerreny.js
+++ AlphaDemo/Scripts/TancarTerreny.js
@@ -1,6 +1,4 @@
 
-var cube : Rigidbody;
-
 private var restSeconds : int;
 private var displaySeconds : int;
 private var displayMinutes : int;
@@ -9,7 +7,7 @@ private var roundedRestSeconds : int;
 
 
 private var screenPos;
-private var ray;
+private var ray : Ray;
 private var hit : RaycastHit;
 private var vista_previa : boolean;
 
@@ -31,7 +29,6 @@ var tile : Transform;
 var width : int;
 var height : int;
 static var creat = false;
-var film : Camera ;
 
 
 //Colocar el contorn de la muralla.
@@ -41,12 +38,12 @@ function Update(){
 	//es poden posar cubs
 	//si el temps es 0 s'han de canviar les textures dels cubs per parets 
 	screenPos = Input.mousePosition;
-	ray = film.ScreenPointToRay(screenPos);
+	ray = Camera.main.ScreenPointToRay(screenPos);
 	
 	if(Input.GetButtonDown("Fire1")){
-		//screenPos = Input.mousePosition;
-		//ray = film.ScreenPointToRay(screenPos);
+		
 		if(Physics.Raycast(ray,hit)){
+			Debug.Log("I hit at: x: "+hit.point.x+" y: "+hit.point.y+" z: "+hit.point.z);
 			hit.point.x = Mathf.Round(hit.point.x);
 			hit.point.y = Mathf.Round(hit.point.y);
 			hit.point.z = Mathf.Round(hit.point.z);
@@ -55,9 +52,6 @@ function Update(){
 				GenerateHashMap();	
 			}else{
 				 if((hit.collider.tag == "Plane")||(hit.collider.tag == "Untagged")){
-				//PlaceCube(hit);
-				//boolmatrix[zgrid-hit.point.z][hit.point.x-xgrid] = true;<------------------------------------------
-				//solid = cubes[Random.Range(0,cubes.length)];<------------------------------------------
 				
 					figura=ComprovarElement(preview);
 				
@@ -107,6 +101,18 @@ function DestroyPreview(){
 
 function ComprovarElement(element:Rigidbody){
 	switch (element.tag){
+		case ("Cub"):
+			Debug.Log("És el Cub");
+			figura = new Array(2);
+			figura[0] = new Array(2);
+			figura[1] = new Array(2);
+			for(i=0;i<figura.length;i++){
+				for(j=0;j<figura[i].length;j++){
+					figura[i][j]=true;
+				}
+			}
+			break;
+	
 		case ("IHoritzontal"):
 			Debug.Log("És la IHoritzontal");
 			figura = new Array(1);
@@ -189,6 +195,24 @@ function ComprovarElement(element:Rigidbody){
 				}	
 			}
 			break;
+			
+		case ("LInvertida"):
+			Debug.Log("És la LInvertida");
+			figura = new Array(3);
+						
+			figura[0] = new Array(2);
+			figura[1] = new Array(2);
+			figura[2] = new Array(2);
+			for(i=0;i<figura.length;i++){
+				for(j=0;j<figura[i].length;j++){
+					if(i<=1 && j==0)
+						figura[i][j]=false;
+					else
+						figura[i][j]=true;
+				}
+			}
+			
+			break;
 		
 		
 		case ("LInvertidaHoritzontal"):
@@ -218,24 +242,6 @@ function ComprovarElement(element:Rigidbody){
 				}
 			}
 			break;
-
-		case ("LInvertida"):
-			Debug.Log("És la LInvertida");
-			figura = new Array(3);
-						
-			figura[0] = new Array(2);
-			figura[1] = new Array(2);
-			figura[2] = new Array(2);
-			for(i=0;i<figura.length;i++){
-				for(j=0;j<figura[i].length;j++){
-					if(i<=1 && j==0)
-						figura[i][j]=false;
-					else
-						figura[i][j]=true;
-				}
-			}
-			
-			break;
 			
 		case ("LInvertidaGanxoHoritzontal"):
 			Debug.Log("És la LInvertidaGanxoHoritzontal");
@@ -251,18 +257,6 @@ function ComprovarElement(element:Rigidbody){
 				}
 			}
 			break;
-				
-		case ("Cub"):
-			Debug.Log("És el Cub");
-			figura = new Array(2);
-			figura[0] = new Array(2);
-			figura[1] = new Array(2);
-			for(i=0;i<figura.length;i++){
-				for(j=0;j<figura[i].length;j++){
-					figura[i][j]=true;
-				}
-			}
-			break;
 		
 		case ("T"):
 			Debug.Log("És la T");
@@ -272,6 +266,21 @@ function ComprovarElement(element:Rigidbody){
 			for(i=0;i<figura.length;i++){
 				for(j=0;j<figura[i].length;j++){
 					if(i==1 && j==0)
+						figura[i][j]=false;
+					else
+						figura[i][j]=true;
+				}
+			}
+			break;
+			
+		case ("TAmunt"):
+			Debug.Log("És la TAmunt");
+			figura = new Array(2);
+			figura[0] = new Array(3);
+			figura[1] = new Array(3);
+			for(i=0;i<figura.length;i++){
+				for(j=0;j<figura[i].length;j++){
+					if(i==0 && (j==0 || j==2))
 						figura[i][j]=false;
 					else
 						figura[i][j]=true;
@@ -292,14 +301,15 @@ function ComprovarElement(element:Rigidbody){
 			}
 			break;
 			
-		case ("TAmunt"):
-			Debug.Log("És la TAmunt");
-			figura = new Array(2);
-			figura[0] = new Array(3);
-			figura[1] = new Array(3);
+		case ("TVerticalInvertida"):
+			Debug.Log("És la TVerticalInvertida");
+			figura = new Array(3);
+			for(i=0;i<figura.length;i++){
+				figura[i] = new Array(2);
+			}
 			for(i=0;i<figura.length;i++){
 				for(j=0;j<figura[i].length;j++){
-					if(i==0 && (j==0 || j==2))
+					if((i==0 || i==2) && (j==0))
 						figura[i][j]=false;
 					else
 						figura[i][j]=true;
@@ -408,37 +418,6 @@ function colocarElement(matriuZona:Array, matriuNouElement:Array, posY:int, posX
 }
 
 
-
-function PlaceCube(hit: RaycastHit) {
-	//Si es pressiona el botó "Fire1" (Disparar per defecte a Unity)
-	//Capturem la posició actual del punter del ratolí a la pantalla
-	//if(Input.GetButtonDown("Fire1")){
-		//var screenPos = Input.mousePosition;<--------------------------------
-		//definim un "ray" que tindrà les mateixes coordenades
-		//que les del ratolí ( coordenades de pantalla
-		//var ray = camera.ScreenPointToRay(Input.mousePosition);<-----------------
-		//variable que retornarà la informació de l'objecte que ha interseccionat amb el ray
-		//Passem la posició del punter de la pantalla a les coordenades del món 3D 
-		//si el raycast toca algun objecte del món 3D, es guardarà a la variable hit
-		//if(Physics.Raycast(ray,hit)){	<----------------------------
-			//var llistagrids = new GridGenerator();<------------------
-			//llistagrids.getList();<--------------------------
-			/*for(var i=0;i<llistagrids.size();i++){
-				if(hit.point == llistagrids.GetIndex(i).position){
-				}
-					
-			}*/
-			/*var x = Mathf.Round(hit.point.x);
-			var y = Mathf.Round(hit.point.y);
-			var z = Mathf.Round(hit.point.z);*/
-			//Debug.Log("I hit at:"+hit.transform.tag);
-			//Debug.Log("I hit at: "+" x: "+x+" y: "+y+" z: "+z);
-			//i s'instanciarà a la posició on ha tocat el ray.	
-			
-		//}
-	//}
-}
-
 function OnGUI () {
     //make sure that your time is based on when this script was first called
     //instead of when your game started
@@ -485,13 +464,8 @@ function GenerateHashMap(){
 
 function GenerateTiles(hit : RaycastHit){
 	// Set Tiles
-	//var xhit = (hit.point.x);
-	//var zhit = (hit.point.z);
-	/*var xhit = Mathf.Round(hit.point.x);
-	var zhit = Mathf.Round(hit.point.z);*/
 	xgrid = hit.point.x -((width)/2);
 	zgrid = hit.point.z +(height/2);
-	Debug.LogWarning("xgrid: "+xgrid+" "+"zgrid: "+zgrid);
 	for(var x = (hit.point.x -((width-1)/2)); x < (hit.point.x+(width/2)); x++){
 		for(var z = (hit.point.z-((height-1)/2)); z < (hit.point.z+(height/2)); z++){
 			var tiler = Instantiate(tile,Vector3(x,0,z),Quaternion.identity);
